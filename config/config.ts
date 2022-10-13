bf58174d8557d3cbe0e8b757ccd1f4b70bc3c03d
weymar <weymar.vargas@eventsite.com.co>
2022-10-13 21:20:45 +0200

    commit inicial

---
+++ config/config.ts
@@ -0,0 +1,8 @@
+const config = {};
+
+config.SEED = 'policy#manager&version-01@';
+config.TIME = 14400;
+config.MONGO_PORT = 27017;
+config.DB_CNN = 'mongodb+srv://policymanager:Policy*123.@cluster0.frbag6k.mongodb.net/zeus'
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors')({origin: true});
+const bodyParser = require('body-parser');
+const app = express();
+
+const connection = require('./conexion.ts')
+// const usuarioRoutes = require('./routes/usuario.router');
+//const authRoutes = require('./routes/auth.router');
+
+require('dotenv').config({path: '.env'});
+
+app.use(cors);
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+//app.use('/login', authRoutes);
+app.get('/', async (req, res) => {
+    return res.status(200).send('Policy manager');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    connection();
+    console.log(`Server running on port: ${port}`);
+});
+
+
+
