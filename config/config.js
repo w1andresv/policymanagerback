de1925c11656d9424cda4f978e6c389cc7924290
weymar <w1andresv@gmail.com>
2022-10-16 11:29:12 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ config.TIME = 14400;
 config.MONGO_PORT = 27017;
 config.DB_CNN = 'mongodb+srv://policymanager:Policy*123.@cluster0.frbag6k.mongodb.net/zeus'
 // upload files
-config.CLIENT_ID='553189609166-eiufk6v42l3la2gfugvbq2apum3ck3sk.apps.googleusercontent.com';
-config.CLIENT_SECRET='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';
+config.CLIENT_ID='';
+config.CLIENT_SECRET='';
 config.REDIRECT_URI='https://developers.google.com/oauthplayground';
-config.REFRESH_TOKEN='1//04BIE1shw1TGvCgYIARAAGAQSNwF-L9Ir_hlmRMLTjEOQ3fhgtbsnNIZBIAUE3g7Rw2FGZjZ6ShIjtdaIL4FsZWq5hIB0OkuefSM';
+config.REFRESH_TOKEN='';
 module.exports = config;
