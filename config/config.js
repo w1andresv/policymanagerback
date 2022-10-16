2403f6973a6de59f5b3cbec7db1201abad7b1ede
weymar <weymar.vargas@eventsite.com.co>
2022-10-16 11:00:43 +0200

    fileuploaddrive

--- config/config.js
+++ config/config.js
@@ -4,5 +4,9 @@ config.SEED = 'policy#manager&version-01@';
 config.TIME = 14400;
 config.MONGO_PORT = 27017;
 config.DB_CNN = 'mongodb+srv://policymanager:Policy*123.@cluster0.frbag6k.mongodb.net/zeus'
-
+// upload files
+config.CLIENT_ID='553189609166-eiufk6v42l3la2gfugvbq2apum3ck3sk.apps.googleusercontent.com';
+config.CLIENT_SECRET='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';
+config.REDIRECT_URI='https://developers.google.com/oauthplayground';
+config.REFRESH_TOKEN='1//04BIE1shw1TGvCgYIARAAGAQSNwF-L9Ir_hlmRMLTjEOQ3fhgtbsnNIZBIAUE3g7Rw2FGZjZ6ShIjtdaIL4FsZWq5hIB0OkuefSM';
 module.exports = config;
--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const cors = require('cors')({origin: true});
 const bodyParser = require('body-parser');
 const app = express();
-
+var fileUpload = require('express-fileupload');
 const connection = require('./conexion')
 // const usuarioRoutes = require('./routes/usuario.router');
 const authRoutes = require('./routes/auth.router');
 const themeRoutes = require('./routes/theme.router');
+const uploadRoutes = require('./routes/adjunto.router');
 
 require('dotenv').config({path: '.env'});
 
 app.use(cors);
+app.use(fileUpload());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 app.use('/auth', authRoutes);
 app.use('/theme', themeRoutes);
+app.use('/uploadfile', uploadRoutes);
 app.get('/', async (req, res) => {
     return res.status(200).send('Policy manager');
 });
